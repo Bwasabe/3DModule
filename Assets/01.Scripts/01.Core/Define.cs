50d4e09ec57733c69b984e2f7a8249b8d537ba9d
박상빈 <jixg20@gmail.com>
2022-03-01 21:56:22 +0900

    매인캠 디파인

---
+++ Assets/01.Scripts/01.Core/Define.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Define
+{
+    public static Camera MainCam{
+        get{
+            if(_mainCam == null){
+                _mainCam = Camera.main;
+            }
+            return _mainCam;
+        }
+
+    }
+
+    private static Camera _mainCam;
+}
