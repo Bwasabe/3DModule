c35584e2a8a5ea5dfdc2e52c035be7b922be112c
박상빈 <jixg20@gmail.com>
2022-03-12 14:09:14 +0900

    이벤트매니저 추가

--- Assets/01.Scripts/01.Core/Define.cs
+++ Assets/01.Scripts/01.Core/Define.cs
@@ -1,12 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public static class Define
 {
-    public static Camera MainCam{
-        get{
-            if(_mainCam == null){
+    public static Camera MainCam
+    {
+        get
+        {
+            if (_mainCam == null)
+            {
                 _mainCam = Camera.main;
             }
             return _mainCam;
